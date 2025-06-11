baf6981776ac1f3f22f6de463fc4da6618b1e9ea
wei4r <hankwei02151@gmail.com>
2025-06-11 23:04:11 +0800

    Refactor Home component to remove unused props

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import FirstPage from "./components/FirstPage";
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 
-export default function Home({ Component, pageProps }) {
+export default function Home() {
   return (
     <Provider store={store}>
       <main className="flex min-h-screen flex-col w-full max-w-full">
