c14ed2dc0aa8f38fd3542f1b7af793ad9394c9d1
wei4r <hankwei02151@gmail.com>
2025-06-14 16:31:54 +0800

    Add CLAUDE.md to .gitignore; remove height styling from FirstPage component

--- src/app/components/FirstPage.jsx
+++ src/app/components/FirstPage.jsx
@@ -5,7 +5,7 @@ import AchievementsSection from './AchievementsSection';
 
 const FirstPage = () => {
   return(
-    <div className="pb-24 h-[calc(100vh-4rem)]">
+    <div className="pb-24">
       <HeroSection />
       <AchievementsSection />
     </div>
